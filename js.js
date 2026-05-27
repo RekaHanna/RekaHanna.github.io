402c11f6af96921ff75c2349277055a886674f38
RekaHanna <151775498+RekaHanna@users.noreply.github.com>
2026-05-27 13:33:37 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,9 +1,17 @@
 async function getCharacter(){
 
   const id = Math.floor(Math.random() * 826) + 1;
-
+ // Véletlen szám generálása 1 és 826 között
+  // Math.random() -> 0 és 1 közötti szám
+  // * 826 -> 0–825 közé skálázza
+  // Math.floor() -> lefelé kerekít egész számra
+  // +1 -> hogy 1-től induljon
   const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+    // Lekérés küldése az API-nak a kiválasztott karakterhez
+  // await megvárja, amíg megérkezik a válasz
   const data = await res.json();
+  // A válasz JSON formátumának átalakítása JavaScript objektummá
+  // await megvárja az átalakítást
 
   document.getElementById("card").innerHTML = `
 
